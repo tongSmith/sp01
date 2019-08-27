12d07d0a4314c404c492539d52c49b2fe8a52acd
tongsmith <wankyqi@sina.com>
2019-08-27 15:01:11 +0800

    sp02itemservice

---
+++ sp02-itemservice/src/main/java/com/tedu/sp02/item/controller/ItemController.java
@@ -0,0 +1,53 @@
+package com.tedu.sp02.item.controller;
+
+import java.util.List;
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tedu.sp01.pojo.Item;
+import com.tedu.sp01.service.ItemService;
+import com.tedu.web.util.JsonResult;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author :mdzz
+ * @description :
+ * @date :2019/8/22 10:50
+ */
+@RestController
+@Slf4j
+public class ItemController {
+    @Autowired
+    private ItemService itemService;
+
+    @Value("${server.port}")
+    private int port;
+
+    @GetMapping("/{orderId}")//处理get请求
+    public JsonResult<List<Item>> getItems(@PathVariable String orderId) throws InterruptedException {
+        log.info("server.port="+port+", orderId="+orderId);
+        ///--设置随机延迟
+        long t = new Random().nextInt(5000);
+        if(Math.random()<0.6) {
+            log.info("item-service-"+port+" - 暂停 "+t);
+            Thread.sleep(t);
+        }
+        List<Item> items = itemService.getItems(orderId);
+        return JsonResult.ok(items).msg("port="+port);
+    }
+
+    @PostMapping("/decreaseNumber")//处理post请求
+    public JsonResult decreaseNumber(@RequestBody List<Item> items) {//@RequestBody从请求体中获取数据
+        log.info("减少商品库存");
+        itemService.decreaseNumbers(items);
+        return JsonResult.ok();
+    }
+}
